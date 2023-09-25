d03d58f9a4251d63636d333ff3ab2f0fb6b767ae
Raphaël Rebel <raphaelrebel@MBP-van-Raphael.codedazur.com>
2023-09-25 11:54:38 +0200

    Added more features

--- src/Components/CardTodo/CardTodo.tsx
+++ src/Components/CardTodo/CardTodo.tsx
@@ -1,105 +1,127 @@
-import { FunctionComponent, ReactNode, useEffect, useState } from "react"
-import "./CardTodo.css"
-import TodoData from "../../Data/TodoData"
-import { EntryType } from "perf_hooks"
+import { FunctionComponent, ReactNode, useEffect, useState } from "react";
+import "./CardTodo.css";
+import TodoData from "../../Data/TodoData";
+import { EntryType } from "perf_hooks";
+import { AnimatePresence, motion } from "framer-motion";
+import TodoItem from "../TodoItem/TodoItem";
 
-
-
-// type ArticleProps = {
-//     className: string,
-//     children?: ReactNode
-// }
-
-// const Article = ({className, children}: ArticleProps) => <article className={className}>{children}</article>
-
-
-
-interface TodoDataProps {
-    name: string,
-    id: number,
-    done: boolean
+export interface TodoDataProps {
+  name: string;
+  id: number;
+  removeFromList?: (id: number) => void;
 }
 
-interface removeFromList {
-    removeFromList: (id: number) => void
-}
+const CardTodo = ({ removeCard, id }: any) => {
+  const [todos, setTodos] = useState<TodoDataProps[]>(TodoData);
+  const [inputValue, setInputValue] = useState<string>("");
+  const [inputTitle, setInputTitle] = useState<boolean>(false);
+  const [title, setTitle] = useState<string>("");
+  const [titleValue, setTitleValue] = useState<string>("");
 
-const TodoItem = ({ name, id, removeFromList }: TodoDataProps & removeFromList) => {
+  const removeFromList = (todoId: number) => {
+    const idCheck = todoId;
+    const updatedTodos = todos.filter((todos) => todos.id !== idCheck);
+    setTodos(updatedTodos);
+  };
 
+  const inputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
 
-    const [isDone, setIsDone] = useState<boolean>(false)
+  const inputChangeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitleValue(event.target.value);
+  };
 
+  const keyDown = (event: any) => {
+    if (event.keyCode == 13 && inputValue !== "") {
+      // const latestId = todos.length - 1
+      // newTodoId = todos[latestId].id + 1
+      const newTodoId = (todos[todos.length - 1]?.id ?? 0) + 1;
 
-    const removeTodo = () => {
-        setIsDone(!isDone);
-        removeFromList(id)
-    };
+      const newInputValue = {
+        name: inputValue,
+        id: newTodoId,
+      };
 
-
-
-
-    return <li className={isDone ? "cardTodo--done" : ""} onClick={removeTodo} key={id}>{name}</li>
-}
-
-const CardTodo = () => {
-    const [todos, setTodos] = useState<TodoDataProps[]>(TodoData)
-    const [inputValue, setInputValue] = useState<string>("")
-
-    const removeFromList = (todoId: number) => {
-        const idCheck = todoId
-        const updatedTodos = todos.filter(todos => todos.id !== idCheck)
-        setTodos(updatedTodos)
+      setTodos(todos.concat(newInputValue));
+      setInputValue("");
     }
+  };
 
-    const inputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setInputValue(event.target.value)
+  const TodoDataToBeRendered = todos.map((todo: TodoDataProps) => {
+    return <TodoItem key={todo.id} removeFromList={removeFromList} {...todo} />;
+  });
+
+  const onkeydownTitle: any = (event: any) => {
+    if (event.keyCode == 13) {
+      setTitle(titleValue);
+      setInputTitle(false);
     }
+  };
 
-    const keyDown = (event: any) => {
-        if (event.keyCode == 13) {
+  let titleInputToBeRendered: any = title ? title : "Things to do";
 
-            let newTodoId = 0
-            if (todos.length !== 0) {
-                const latestId = todos.length - 1
-                newTodoId = todos[latestId].id + 1
-            } else {
-                newTodoId = 1
-            }
+  let nameThisBetter: React.ReactNode = inputTitle ? (
+    <motion.input
+      value={titleValue}
+      onChange={inputChangeTitle}
+      onKeyDown={onkeydownTitle}
+      type="text"
+      className="title_input input"
+      initial={{ y: -20, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      exit={{ y: 20, opacity: 0 }}
+    />
+  ) : (
+    titleInputToBeRendered
+  );
 
+  const titleInput: () => void = () => {
+    setInputTitle(true);
+  };
 
+  return (
+    <motion.article
+      animate={{ opacity: 1, x: 0 }}
+      initial={{ opacity: 0, x: -40 }}
+      transition={{ delay: 0.5 }}
+      exit={{ opacity: 0, scale: 0.5, x: -40 }}
+      className="cardTodo"
+    >
+      <header>
+        <motion.h2
+          onClick={titleInput}
+          initial={{ y: -20 }}
+          animate={{ y: 0 }}
+          exit={{ y: 20 }}
+        >
+          {nameThisBetter}
+        </motion.h2>
+        <p onClick={titleInput} className="cardTodo_edit">
+          Edit
+        </p>
+        <p
+          onClick={() => {
+            removeCard(id);
+          }}
+        >
+          X
+        </p>
+      </header>
 
+      <ul>
+        <AnimatePresence>{TodoDataToBeRendered}</AnimatePresence>
+      </ul>
+      <input
+        className="cardTodo_input input"
+        placeholder="Add a todo"
+        value={inputValue}
+        onKeyDown={keyDown}
+        onChange={inputChange}
+        type="text"
+      />
+    </motion.article>
+  );
+};
 
-            const newInputValue = {
-                name: inputValue,
-                done: false,
-                id: newTodoId
-            }
-
-            setTodos(todos.concat(newInputValue))
-            setInputValue("")
-
-        }
-    }
-
-
-
-    const TodoDataToBeRendered = todos.map((todo: TodoDataProps) => {
-        if (!todo.done) {
-            return <TodoItem removeFromList={removeFromList} done={todo.done} name={todo.name} id={todo.id} key={todo.id} />
-        }
-
-    })
-
-    return (
-        <article className="cardTodo">
-            <header>
-                <h2>Things to do</h2>
-            </header>
-            <ul>
-                {TodoDataToBeRendered}
-            </ul>
-            <input value={inputValue} onKeyDown={keyDown} onChange={inputChange} type="text" />
-        </article>)
-}
-
-export default CardTodo
+export default CardTodo;
---
+++ src/Data/TodoCards.tsx
@@ -0,0 +1,12 @@
+export interface TodoCardsProps{
+    id: number
+}
+const TodoCards: TodoCardsProps[] = [
+    {
+        id: 1
+    },
+    
+]
+
+
+export default TodoCards
